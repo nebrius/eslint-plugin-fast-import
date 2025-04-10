b5b85b17791bbeffff232ea35d191f4dd02ff658
nebrius <bryan@nebri.us>
2025-04-10 06:47:01 -0700

    Wrote publish script

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
 # eslint-plugin-fast-import
 
-TODO: make sure to mention that this won't resolve fully and will cause false positives:
-
-```ts
-// foo.ts
-import { join } from './bar';
-
-// bar.ts
-export * from 'node:path';
-```
-
-TODO: create lint rule preventing the above
-
-TODO: mention case-sensitive weirdness when specifying file patterns to CLI, e.g. it works as case insensitive, but also doesn't
-
-TODO: mention limitation in parsing tsconfig.json that we don't support multiple paths per alias
-
-TODO: mention limitation of CLI and first-run editor parsing where syntax errors disable this plugin
+This project is in early beta. Documentation coming soon!
---
+++ scripts/publish.ts
@@ -0,0 +1,83 @@
+import { spawn } from 'child_process';
+import { simpleGit } from 'simple-git';
+import { getDirname } from 'cross-dirname';
+import { join, resolve } from 'node:path';
+import { readFileSync } from 'node:fs';
+import readLine from 'node:readline/promises';
+
+const ROOT_DIR = resolve(getDirname(), '..');
+const DRY_RUN = process.argv.includes('--dry-run');
+
+const version = JSON.parse(
+  readFileSync(join(getDirname(), '..', 'package.json'), 'utf-8')
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+).version as string;
+
+const git = simpleGit(ROOT_DIR);
+
+if (DRY_RUN) {
+  console.log(`Performing dry run of publishing version ${version}`);
+} else {
+  const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const response = await rl.question(
+    `Actually publishing version ${version} to npm. Are you sure [y/N]? `
+  );
+  if (response !== 'y' && response !== 'Y') {
+    process.exit(-1);
+  }
+  rl.close();
+}
+
+if (!(await git.status()).isClean()) {
+  console.error(`Git status is not clean`);
+  if (!DRY_RUN) {
+    process.exit(-1);
+  }
+}
+
+function runStep(command: string, args: string[]): Promise<void> {
+  return new Promise((resolve) => {
+    spawn(command, args, {
+      cwd: ROOT_DIR,
+      stdio: 'inherit',
+    }).on('exit', (code) => {
+      if (code) {
+        if (!DRY_RUN) {
+          process.exit(-1);
+        }
+      }
+      resolve();
+    });
+  });
+}
+
+// build
+await runStep('npm', ['run', 'build']);
+
+// lint
+await runStep('npm', ['run', 'lint']);
+
+// test
+await runStep('npm', ['run', 'test']);
+
+// npm publish
+if (DRY_RUN) {
+  await runStep('npm', ['publish', '--dry-run']);
+} else {
+  await runStep('npm', ['publish']);
+}
+
+// git tag
+console.log(`Tagging git with version ${version}`);
+if (!DRY_RUN) {
+  await git.addTag(version);
+}
+
+// git push
+console.log(`Pushing tags to GitHub`);
+if (!DRY_RUN) {
+  await git.pushTags();
+}
