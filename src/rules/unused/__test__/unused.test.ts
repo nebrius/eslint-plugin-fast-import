f99c7fdf975357ffd106a14ba59f24d4768d0a77
nebrius <bryan@nebri.us>
2025-04-26 20:02:49 -0700

    More unused imports tests

--- src/rules/unused/__test__/unused.test.ts
+++ src/rules/unused/__test__/unused.test.ts
@@ -1,3 +1,4 @@
+import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
 
 import { RuleTester } from '@typescript-eslint/rule-tester';
@@ -7,6 +8,8 @@ import { noUnusedExports } from '../unused.js';
 
 const TEST_PROJECT_DIR = join(getDirname(), 'project');
 const FILE_A = join(TEST_PROJECT_DIR, 'a.ts');
+const FILE_D_DTS = join(TEST_PROJECT_DIR, 'd.d.ts');
+const FILE_E = join(TEST_PROJECT_DIR, 'e.ts');
 
 const ruleTester = new RuleTester({
   languageOptions: {
@@ -30,6 +33,29 @@ export const a2 = 10;
         'fast-import': {
           rootDir: TEST_PROJECT_DIR,
           mode: 'fix',
+        },
+      },
+    },
+    {
+      code: readFileSync(FILE_D_DTS, 'utf8'),
+      filename: FILE_D_DTS,
+      settings: {
+        'fast-import': {
+          rootDir: TEST_PROJECT_DIR,
+          mode: 'fix',
+        },
+      },
+    },
+    {
+      code: readFileSync(FILE_E, 'utf8'),
+      filename: FILE_E,
+      settings: {
+        'fast-import': {
+          rootDir: TEST_PROJECT_DIR,
+          mode: 'fix',
+          entryPoints: {
+            'e.ts': ['e'],
+          },
         },
       },
     },
@@ -58,6 +84,22 @@ export const a2 = 10;
     export const a3 = 10;
     `,
       filename: FILE_A,
+      errors: [{ messageId: 'noTestOnlyImports' }],
+      settings: {
+        'fast-import': {
+          rootDir: TEST_PROJECT_DIR,
+          mode: 'fix',
+        },
+      },
+    },
+    {
+      code: `export const a3 = 10;`,
+      filename: FILE_A,
+      options: [
+        {
+          allowNonTestTypeExports: false,
+        },
+      ],
       errors: [{ messageId: 'noTestOnlyImports' }],
       settings: {
         'fast-import': {
